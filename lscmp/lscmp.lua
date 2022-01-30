bb74f5542e45f8d38df0c7bb6e0a668bab343ee7
Exotic0015 <jalokim541515@gmail.com>
2022-01-30 19:26:19 +0100

    hnnnnnngh

--- lscmp/lscmp.lua
+++ lscmp/lscmp.lua
@@ -4,12 +4,12 @@ local args, _ = require("shell").parse(...)
 if args[1] == nil then
     local cnt = 1
     for i, v in component.list() do
-		gpu.setForeground(0x2E86C1)
-		io.write(cnt .. "\t")
-		gpu.setForeground(0x2EC173)
-		io.write(i .. "\t")
-		gpu.setForeground(0xFFFFFF)
-		io.write(v .. "\n")
+        gpu.setForeground(0x2E86C1)
+        io.write(cnt .. "\t")
+        gpu.setForeground(0x2EC173)
+        io.write(i .. "\t")
+        gpu.setForeground(0xFFFFFF)
+        io.write(v .. "\n")
         cnt = cnt + 1
     end
 else
@@ -18,12 +18,11 @@ else
         if tostring(cnt) == args[1] then
             local dev, reason = component.proxy(component.get(i))
             for a, b in pairs(dev) do
-                --print(a, b)
-				gpu.setForeground(0x2E86C1)
-				io.write(a .. "\t")
-				gpu.setForeground(0x2EC173)
-				io.write(b .. "\n")
-				gpu.setForeground(0xFFFFFF)
+                gpu.setForeground(0x2E86C1)
+                io.write(a .. "\t")
+                gpu.setForeground(0x2EC173)
+                io.write(b .. "\n")
+                gpu.setForeground(0xFFFFFF)
             end
         end
         cnt = cnt + 1
